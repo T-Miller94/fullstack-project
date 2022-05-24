b24153e361a969a57967ce556b7e22b23ca26453
T-Miller94 <miller.thomas1337@gmail.com>
2022-05-24 16:09:41 -0500

    user search button: added functionality

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 //declarations
 const body = document.querySelector('body')
+const main = document.querySelector('#main')
 const userSearch = document.querySelector('#user_search')
 const newUser = document.querySelector('#new_user')
 const stats = document.querySelector('#user_stats')
@@ -12,7 +13,26 @@ newUser.addEventListener('click', newUserButton)
 
 //callback functions
 function userSearchButton() {
-    console.log('Working')
+    let searchPopUp = document.createElement('div')
+    searchPopUp.id = 'user_popup'
+        let searchContainer = document.createElement('div')
+        searchContainer.id = 'search_container'
+            let name = document.createElement('input')
+                name.id = 'name'
+            let password = document.createElement('input')
+                password.id = 'password'
+            let email = document.createElement('input')
+                email.id = 'email'
+            let search = document.createElement('button')
+                search.id = 'search'
+                search.innerText = 'Search'
+            searchContainer.append(name)
+            searchContainer.append(password)
+            searchContainer.append(email)
+            searchContainer.append(search)
+        searchPopUp.append(searchContainer)
+    main.prepend(searchPopUp)
+    
 }
 
 function newUserButton() {
