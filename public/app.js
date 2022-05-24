f0854624cac2121d8ff1c8355273779fafdb9eb2
T-Miller94 <miller.thomas1337@gmail.com>
2022-05-24 15:02:54 -0500

    app.js declarations made

---
+++ public/app.js
@@ -0,0 +1,11 @@
+//declarations
+const body = document.querySelector('body')
+const userSearch = document.querySelector('#user_search')
+const newUser = document.querySelector('#new_user')
+const stats = document.querySelector('#user_stats')
+const searchBar = document.querySelector('#search_bar')
+const resultBox = document.querySelector('#results')
+
+//button functions
+
+//callback functions
