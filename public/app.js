cbd65a061dbc8e6c5ccce8702da91a11e7584079
T-Miller94 <miller.thomas1337@gmail.com>
2022-05-25 15:28:15 -0500

    person get/post reqs working; building towards adding divs

--- public/app.js
+++ public/app.js
@@ -113,7 +113,9 @@ function hidePopUp(e) {
 
 async function findUser(name, id, password) {
     $.get(`https://damp-taiga-73156.herokuapp.com/person/${id}`, (user) => {
-        console.log(user[0])
+        if(name === user[0].name && password === user[0].password) {
+            console.log(user[0])
+        }
     })
 }
 
